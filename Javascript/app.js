2a6450d8b56dc3509c9c5f8b5b993c4c8a1b631f
Cédric <pourriascedric@gmail.com>
2018-04-19 16:10:29 +0200

    App.js, comment l'appeler

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,8 +1,9 @@
-var app = (
+var app = {
     init : function () {
         console.log('Je viens de charger mon fichier JS')
       },
-);
+    };
 
+    
 //J'appel app.init, à la fin du chargment de ma page HTML
 document.addEventListener('DOMContentLoaded', app.init);
